6eaa04a53b5cc0873706df0e45f8cae7ca6432a2
seohyeon2 <seo_hyeon418@naver.com>
2023-08-23 13:24:39 +0900

    design: CalendarView 디자인 수정 #5

--- NaOng/NaOng/WholeToDoView/View/CalendarView.swift
+++ NaOng/NaOng/WholeToDoView/View/CalendarView.swift
@@ -17,48 +17,51 @@ struct CalendarView: View {
     }
     
     var body: some View {
-        VStack {
-            DatePicker(
-                    "Start Date",
-                    selection: $calendarViewModel.date,
-                    displayedComponents: [.date]
-                )
-                .datePickerStyle(.graphical)
-                .tint(.gray)
-                .onChange(of: $calendarViewModel.date.wrappedValue) { newValue in
-                    calendarViewModel.fetchTodoItems()
-                }
-                .padding()
-            
-            List {
-                ForEach($calendarViewModel.toDoItems) { item in
-                    let localNotificationManager = LocalNotificationManager()
-                    let viewModel = ToDoListItemViewModel(toDoItem: item.wrappedValue, viewContext: viewContext, localNotificationManager: localNotificationManager)
-                    ToDoListItemView(toDoListItemViewModel: viewModel)
-                        .listRowSeparator(.hidden)
-                        .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
-                        .overlay {
-//                            NavigationLink {
-//                                let toDoItemDetailViewModel = ToDoItemDetailViewModel(viewContext: viewContext, toDoItem: item.wrappedValue)
-//                                ToDoItemDetailView(toDoItemDetailViewModel: toDoItemDetailViewModel)
-//                            } label: {
-//                                EmptyView()
-//                            }
-//                            .opacity(0)
+        NavigationView {
+            VStack {
+                DatePicker(
+                        "Start Date",
+                        selection: $calendarViewModel.date,
+                        displayedComponents: [.date]
+                    )
+                    .datePickerStyle(.graphical)
+                    .tint(.gray)
+                    .onChange(of: $calendarViewModel.date.wrappedValue) { newValue in
+                        calendarViewModel.fetchTodoItems()
+                    }
+                    .padding()
+                
+                List {
+                    ForEach($calendarViewModel.toDoItems) { item in
+                        let localNotificationManager = LocalNotificationManager()
+                        let viewModel = ToDoListItemViewModel(toDoItem: item.wrappedValue, viewContext: viewContext, localNotificationManager: localNotificationManager)
+                        ToDoListItemView(toDoListItemViewModel: viewModel)
+                            .listRowSeparator(.hidden)
+                            .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
+                            .overlay {
+                                NavigationLink {
+                                    let toDoItemDetailViewModel = ToDoItemDetailViewModel(viewContext: viewContext, toDoItem: item.wrappedValue, localNotificationManager: localNotificationManager)
+                                    ToDoItemDetailView(toDoItemDetailViewModel: toDoItemDetailViewModel)
+                                } label: {
+                                    EmptyView()
+                                }
+                                .opacity(0)
+                            }
+                            .background(Color("secondary"))
+                    }
+                    .onDelete { indexSet in
+                        withAnimation {
+                            calendarViewModel.deleteItems(offsets: indexSet)
                         }
-                        .background(Color("secondary"))
-                }
-                .onDelete { indexSet in
-                    withAnimation {
-                        calendarViewModel.deleteItems(offsets: indexSet)
                     }
                 }
+                .listStyle(.plain)
+                .buttonStyle(.plain)
+                .background(Color("secondary"))
+                .frame(width: UIScreen.main.bounds.width)
             }
-            .listStyle(.plain)
-            .buttonStyle(.plain)
-            .background(Color("secondary"))
-            .frame(width: UIScreen.main.bounds.width)
         }
+        
         .overlay {
             Button {
                 print($calendarViewModel.date)
@@ -83,8 +86,3 @@ struct CalendarView: View {
     }
 }
 
-//struct CalendarView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        CalendarView()
-//    }
-//}
