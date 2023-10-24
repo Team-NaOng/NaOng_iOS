d22f057d81a2e2c482c4096c41a6b44824f6e9bd
seohyeon2 <seo_hyeon418@naver.com>
2023-10-24 17:19:48 +0900

    design: DetailView 디자인 변경 #14
    - 기존에 DetailView에서 바로 수정할 수 있던 구조에서 데이터값만 보여지도록 변경
    - 수정은 navigationBarItems의 오른쪽 버튼을 클릭해서 변경하도록 함

--- NaOng/NaOng/TodayToDoView/View/ToDoItemDetailView.swift
+++ NaOng/NaOng/TodayToDoView/View/ToDoItemDetailView.swift
@@ -8,8 +8,8 @@
 import SwiftUI
 
 struct ToDoItemDetailView: View {
-    @Environment(\.presentationMode) var presentationMode
-    
+    @Environment(\.managedObjectContext) var viewContext
+
     @ObservedObject private var toDoItemDetailViewModel: ToDoItemDetailViewModel
     
     init(toDoItemDetailViewModel: ToDoItemDetailViewModel) {
@@ -21,35 +21,50 @@ struct ToDoItemDetailView: View {
             ToDoViewFactory.makeToDoMoldView(
                 content: ToDoViewFactory.makeToDoTextEditor(
                     title: "할 일 내용",
-                    text: $toDoItemDetailViewModel.content),
+                    text: .constant(toDoItemDetailViewModel.toDoItem.content ?? "")),
                 height: 200
             )
-
+            
             ToDoViewFactory.makeToDoMoldView(
                 content: ToDoViewFactory.makeToDoToggle(
-                    isOn: $toDoItemDetailViewModel.isRepeat,
+                    isOn: .constant(toDoItemDetailViewModel.toDoItem.isRepeat),
                     title: "반복 여부")
             )
-             
+            
             ToDoViewFactory.makeToDoMoldView(
                 content: ToDoViewFactory.makeToDoPicker(
                     title: "알림 타입",
-                    selection: $toDoItemDetailViewModel.alarmType)
+                    selection:  .constant(toDoItemDetailViewModel.toDoItem.alarmType ?? "위치"))
             )
             
-            if toDoItemDetailViewModel.alarmType == "위치" {
-//                ToDoViewFactory.makeToDoMoldView(
-//                    content: ToDoViewFactory.makeAlarmLocationView(
-//                        title: "알림 위치",
-//                        selectedLocation: "도로명 주소 수정하기",
-//                        destination: LocationSelectionView(locationSelectionViewModel: LocationSelectionViewModel(viewContext: Location.viewContext), path: $path))
-//                )
+            ToDoViewFactory.makeToDoMoldView(
+                content: ToDoViewFactory.makeToDoDatePicker(
+                    selection: .constant(toDoItemDetailViewModel.toDoItem.alarmTime ?? Date()),
+                    title: "알림 날짜",
+                    displayedComponent: .date)
+            )
+            
+            if toDoItemDetailViewModel.toDoItem.alarmType == "위치" {
+                ToDoViewFactory.makeToDoMoldView(
+                    content:
+                        VStack() {
+                            Text("알림 위치 \n")
+                                .frame(maxWidth: .infinity, alignment: .topLeading)
+                            // TODO: 수정
+                            Text("알람 위치 들어갈 자리")
+                                .lineLimit(2)
+                                .frame(maxWidth: .infinity, alignment: .topLeading)
+                        }
+                        .font(.custom("Binggrae", size: 15))
+                        .frame(width: UIScreen.main.bounds.width - 80, alignment: .top),
+                    height: 100
+                )
             } else {
                 ToDoViewFactory.makeToDoMoldView(
                     content: ToDoViewFactory.makeAlarmTimeView(
-                        selection: $toDoItemDetailViewModel.alarmTime,
+                        selection: .constant(toDoItemDetailViewModel.toDoItem.alarmTime ?? Date()),
                         title: "알림 시간",
-                        displayedComponent: [.hourAndMinute, .date])
+                        displayedComponent: .hourAndMinute)
                 )
             }
             
@@ -59,23 +74,15 @@ struct ToDoItemDetailView: View {
         .navigationBarItems(
             trailing:
                 Button(action: {
-                    toDoItemDetailViewModel.EditToDo()
-                    presentationMode.wrappedValue.dismiss()
+                    toDoItemDetailViewModel.showingToDoItemAddView = true
                 }, label: {
-                    Text("저장")
+                    Text("수정")
                 })
                 .frame(width: 50, height: 50)
+                .fullScreenCover(isPresented: $toDoItemDetailViewModel.showingToDoItemAddView) {
+                    let viewModel = ToDoItemAddViewModel(viewContext: viewContext, localNotificationManager: toDoItemDetailViewModel.localNotificationManager)
+                    ToDoItemAddView(toDoItemAddViewModel: viewModel)
+                }
         )
-        .onAppear {
-            toDoItemDetailViewModel.setUpToDoFormData()
-        }
     }
 }
-
-/*
-struct ToDoItemDetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        ToDoItemDetailView()
-    }
-}
-*/
--- NaOng/NaOng/TodayToDoView/ViewModel/ToDoItemDetailViewModel.swift
+++ NaOng/NaOng/TodayToDoView/ViewModel/ToDoItemDetailViewModel.swift
@@ -9,41 +9,16 @@ import Foundation
 import CoreData
 
 class ToDoItemDetailViewModel: ObservableObject {
-    @Published var content: String = ""
-    @Published var alarmType: String = ""
-    @Published var alarmTime: Date = Date()
-    @Published var isRepeat: Bool = false
+    @Published var showingToDoItemAddView: Bool = false
     
-    private var toDoItem: ToDo
+    private(set) var toDoItem: ToDo
     private let viewContext: NSManagedObjectContext
-    private let localNotificationManager: LocalNotificationManager
+    private(set) var localNotificationManager: LocalNotificationManager
     
     init(viewContext: NSManagedObjectContext, toDoItem: ToDo, localNotificationManager: LocalNotificationManager) {
         self.viewContext = viewContext
         self.toDoItem = toDoItem
         self.localNotificationManager = localNotificationManager
-        self.setUpToDoFormData()
-    }
-    
-    func EditToDo() {
-        do {
-            toDoItem.content = content
-            toDoItem.alarmType = alarmType
-            toDoItem.alarmTime = alarmTime
-            toDoItem.isRepeat = isRepeat
-            
-            try toDoItem.save(viewContext: viewContext)
-        } catch {
-            print("error!")
-        }
-        
-        localNotificationManager.editLocalNotification(toDo: toDoItem)
-    }
-    
-    func setUpToDoFormData() {
-        self.content = self.toDoItem.content ?? ""
-        self.alarmType = self.toDoItem.alarmType ?? "위치"
-        self.alarmTime = self.toDoItem.alarmTime ?? Date()
-        self.isRepeat = self.toDoItem.isRepeat
     }
 }
+
